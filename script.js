fefbe434f2b4f2bd787ea3552b713677211b852c
masrafialamsajib <masrafialammas37@gmail.com>
2023-12-12 05:28:02 +0600

    final touch

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function openmenu(){
 }function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+
+        const scriptURL = '<https://script.google.com/macros/s/AKfycbxViqq-FxkrTDpwIgGh_qvKGd2yU2UEhpULLpQiNoIPWHePVzo_q_0kubYO_1JS4Gb6uw/exec>'
+        const form = document.forms['submit-to-google-sheet']
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => console.log('Success!', response))
+            .catch(error => console.error('Error!', error.message))
+        })
